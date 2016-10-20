04ccc9cf616fec188161d6342821b4fcf4597b31
Phillip English <vitkiarts@gmail.com>
2016-10-19 20:44:42 -0400

    added ItemViewModel file

---
+++ ViaflyProject/ItemViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ItemViewModel.swift
+//  ViaflyProject
+//
+//  Created by Phillip English on 10/19/16.
+//  Copyright © 2016 Vitki Arts. All rights reserved.
+//
+
+import Foundation
+
+struct ItemViewModel {
+    
+    var itemArray: [ItemModel] = []
+    
+    func populateArray() {
+        //let path = Bundle.main.path(forResource: "Inventory", ofType: "csv")
+        //let importer = CSVImporter<[String: String]>(path: path)
+        
+    }
+
+}
